3b2900aeca0936e44130f68b0fc70a81cf984f96
Vladimir <vladimir.jaksic@gmail.com>
2018-04-07 01:09:29 +0200

    reverse data pipe initial implementation

--- revdatapipe.c
+++ revdatapipe.c
@@ -1,81 +1,32 @@
-/*
- * Datapipe - Create a listen socket to pipe connections to another
- * machine/port. 'localport' accepts connections on the machine running    
- * datapipe, which will connect to 'remoteport' on 'remotehost'.
- * It will fork itself into the background on non-Windows machines.
- *
- * This implementation of the traditional "datapipe" does not depend on
- * forking to handle multiple simultaneous clients, and instead is able
- * to do all processing from within a single process, making it ideal
- * for low-memory environments.  The elimination of the fork also
- * allows it to be used in environments without fork, such as Win32.
- *
- * This implementation also differs from most others in that it allows
- * the specific IP address of the interface to listen on to be specified.
- * This is useful for machines that have multiple IP addresses.  The
- * specified listening address will also be used for making the outgoing
- * connections on.
- *
- * Note that select() is not used to perform writability testing on the
- * outgoing sockets, so conceivably other connections might have delayed
- * responses if any of the connected clients or the connection to the
- * target machine is slow enough to allow its outgoing buffer to fill
- * to capacity.
- *
- * Compile with:
- *     cc -O -o datapipe datapipe.c
- * On Solaris/SunOS, compile with:
- *     gcc -Wall datapipe.c -lsocket -lnsl -o datapipe
- * On Windows compile with:
- *     bcc32 /w datapipe.c                (Borland C++)
- *     cl /W3 datapipe.c wsock32.lib      (Microsoft Visual C++)
- *
- * Run as:
- *   datapipe localhost localport remotehost remoteport
- *
- *
- * written by Jeff Lawson <jlawson@bovine.net>
- * inspired by code originally by Todd Vierling, 1995.
- */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <time.h>
 #if defined(__WIN32__) || defined(WIN32) || defined(_WIN32)
-  #define WIN32_LEAN_AND_MEAN
-  #include <winsock.h>
-  #define bzero(p, l) memset(p, 0, l)
-  #define bcopy(s, t, l) memmove(t, s, l)
+    #define WIN32_LEAN_AND_MEAN
+    #include <winsock.h>
+    #define bzero(p, l) memset(p, 0, l)
+    #define bcopy(s, t, l) memmove(t, s, l)
 #else
-  #include <sys/time.h>
-  #include <sys/types.h>
-  #include <sys/socket.h>
-  #include <sys/wait.h>
-  #include <netinet/in.h>
-  #include <arpa/inet.h>
-  #include <unistd.h>
-  #include <netdb.h>
-  #include <strings.h>
-  #define recv(x,y,z,a) read(x,y,z)
-  #define send(x,y,z,a) write(x,y,z)
-  #define closesocket(s) close(s)
-  typedef int SOCKET;
+    #include <sys/time.h>
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <sys/wait.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+    #include <netdb.h>
+    #include <strings.h>
+    #define recv(x,y,z,a) read(x,y,z)
+    #define send(x,y,z,a) write(x,y,z)
+    #define closesocket(s) close(s)
+    typedef int SOCKET;
 #endif
 
 #ifndef INADDR_NONE
 #define INADDR_NONE 0xffffffff
 #endif
-
-
-
-#define MAXCLIENTS 20
-#define IDLETIMEOUT 300
-
-
-const char ident[] = "$Id: revdatapipe.c,v 1.0 2018/01/01 01:01:01 h4x0r Exp $";
 
 int main(int argc, char *argv[])
 { 
@@ -92,11 +43,11 @@ int main(int argc, char *argv[])
 #endif
 
 
-  /* check number of command line arguments */
+    /* check number of command line arguments */
     if (5 != argc) 
     {
         fprintf(stderr, "Usage: %s remotehost1 remoteport1 remotehost2 remoteport2\n", argv[0]);
-        return 30;
+        return -1;
     }
 
     /* connect to servers */
@@ -108,7 +59,7 @@ int main(int argc, char *argv[])
         if (!dest[i].sin_port)
         {
             fprintf(stderr, "invalid target port\n");
-            return 25;
+            return -1;
         }
         dest[i].sin_addr.s_addr = inet_addr(argv[1 + i * 2]);
         if (dest[i].sin_addr.s_addr == INADDR_NONE)
@@ -117,7 +68,7 @@ int main(int argc, char *argv[])
             if (NULL == (n = gethostbyname(argv[1 + i * 2])))
             {
               perror("gethostbyname");
-              return 25;
+              return -1;
             }    
             bcopy(n->h_addr, (char *) &dest[i].sin_addr, n->h_length);
         }
@@ -136,15 +87,15 @@ int main(int argc, char *argv[])
         }
     }
 
-  /* fork off into the background. */
+    /* fork off into the background. */
 #if !defined(__WIN32__) && !defined(WIN32) && !defined(_WIN32)
-  if ((i = fork()) == -1) {
-    perror("fork");
-    return 20;
-  }
-  if (i > 0)
-    return 0;
-  setsid();
+    if ((i = fork()) == -1) {
+        perror("fork");
+        return -1;
+    }
+    if (i > 0)
+        return 0;
+    setsid();
 #endif
 
     fd_set fdsr;
@@ -152,12 +103,11 @@ int main(int argc, char *argv[])
     /* main polling loop. */
     while (1)
     {
-        /* build the list of sockets to check. */
         FD_ZERO(&fdsr);
         FD_SET(sockfd[0], &fdsr);
         FD_SET(sockfd[1], &fdsr);
         if (select(maxsock + 1, &fdsr, NULL, NULL, NULL) < 0) {
-            return 30;
+            return -1;
         }
 
 
@@ -181,7 +131,3 @@ int main(int argc, char *argv[])
     
   return 0;
 }
-
-
-
-
